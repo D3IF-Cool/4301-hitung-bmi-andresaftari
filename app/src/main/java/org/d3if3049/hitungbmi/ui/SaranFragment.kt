ca7d6a2b5553ad36b7f4bd33b749ce9023549b17
andresaftari <andresaftari@gmail.com>
2021-03-16 12:59:59 +0700

    3.2 Membuat fragment lihat saran

---
+++ app/src/main/java/org/d3if3049/hitungbmi/ui/SaranFragment.kt
@@ -0,0 +1,51 @@
+package org.d3if3049.hitungbmi.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import org.d3if3049.hitungbmi.R
+import org.d3if3049.hitungbmi.data.KategoriBmi
+import org.d3if3049.hitungbmi.databinding.FragmentSaranBinding
+
+class SaranFragment : Fragment() {
+    private lateinit var binding: FragmentSaranBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSaranBinding.inflate(layoutInflater, container, false)
+        updateUI(KategoriBmi.KURUS)
+        return binding.root
+    }
+
+    private fun updateUI(kategori: KategoriBmi) {
+        val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+        when (kategori) {
+            KategoriBmi.KURUS -> {
+                actionBar?.title = getString(R.string.judul_kurus)
+                with(binding) {
+                    ivBmi.setImageResource(R.drawable.kurus)
+                    tvSaran.text = getString(R.string.saran_kurus)
+                }
+            }
+            KategoriBmi.IDEAL -> {
+                actionBar?.title = getString(R.string.judul_ideal)
+                with(binding) {
+                    ivBmi.setImageResource(R.drawable.ideal)
+                    tvSaran.text = getString(R.string.saran_ideal)
+                }
+            }
+            KategoriBmi.GEMUK -> {
+                actionBar?.title = getString(R.string.judul_gemuk)
+                with(binding) {
+                    ivBmi.setImageResource(R.drawable.gemuk)
+                    tvSaran.text = getString(R.string.saran_gemuk)
+                }
+            }
+        }
+    }
+}
